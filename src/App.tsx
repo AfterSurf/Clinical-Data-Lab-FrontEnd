79092c68de994873b88d1561908d15f925cdcdb2
alexanderlinss <alexanderlinss@MacBooks-MacBook-Pro.local>
2021-09-05 01:55:22 +0200

    request ans backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const onTitleChange = (e:any) => setTitle(e.target.value);
+  const onBodyChange = (e:any) => setBody(e.target.value);
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    console.log("react - handlesubmit()")
+    const data = { title, body };
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    };
+    fetch("http://172.20.10.3:3003/initData", requestOptions)
+      .then(response => 
+        { console.log("im fetch")
+          response.json()})
+      .then(res => console.log("response",res));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <form>
+          <input  placeholder="Title" value={title}
+            onChange={onTitleChange} required />
+          <textarea placeholder="Body" value={body}
+            onChange={onBodyChange} required />
+          <button type="submit" onClick={handleSubmit}>
+           Create Post
+          </button>
+        </form>
+      </div>
   );
 }
 
